98c0a6fa6908d4470aa348fadb1e2384f12ce843
Jonathan Lorentz <lorentzjonathan108@gmail.com>
2024-12-02 00:08:38 -0500

    early sh exit

--- precommit.sh
+++ precommit.sh
@@ -6,6 +6,6 @@ git switch -C docs
 cargo doc --no-deps || exit 1
 echo "<meta http-equiv=\"refresh\" content=\"0; url=reggy\">" > target/doc/index.html || exit 1
 cp -r target/doc ./docs || exit 1
-git add .
-git commit -m "build docs $(date +%Y%m%d%H%M%S)"
+git add . || exit 1
+git commit -m "build docs $(date +%Y%m%d%H%M%S)" || exit 1
 git switch -f main
