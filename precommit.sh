ab989a00a89cf5ecc3800f644801d89c6e62d31c
Jonathan Lorentz <lorentzjonathan108@gmail.com>
2024-12-02 00:26:34 -0500

    set from cli

--- precommit.sh
+++ precommit.sh
@@ -2,8 +2,7 @@ cargo test || exit 1
 cargo clippy || exit 1
 cargo fmt || exit 1
 
-git switch -C docs
-git branch --set-upstream-to=origin/docs docs
+git switch -C docs || exit 1
 cargo doc --no-deps || exit 1
 echo "<meta http-equiv=\"refresh\" content=\"0; url=reggy\">" > target/doc/index.html || exit 1
 cp -r target/doc ./docs || exit 1
