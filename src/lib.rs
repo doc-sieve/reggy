f99f0ff8f438564d28095b08596373cfff5a6e22
Jonathan Lorentz <lorentzjonathan108@gmail.com>
2024-12-03 00:45:42 -0500

    high level api

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,23 @@ mod search;
 
 pub use parser::{Ast, Error};
 pub use search::{Match, Search};
+
+pub struct Pattern {
+    s: Search
+}
+
+impl Pattern {
+    pub fn new(code: &str) -> Result<Self, Error> {
+        let ast = Ast::parse(code)?;
+        Ok(Self {
+            s: Search::new(std::slice::from_ref(&ast))
+        })
+    }
+
+    pub fn findall(&mut self, haystack: &str) -> Vec<(usize, usize)> {
+        let mut res: Vec<_> = self.s.step(haystack).iter().map(|m| m.pos).collect();
+        res.extend(self.s.finish().iter().map(|m| m.pos));
+        self.s.reset();
+        res
+    }
+}
